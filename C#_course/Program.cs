02d652f8ef7442eb1dc1ebb27ba254457795bd35
Avila-Pato <p.avilaf1998@gmail.com>
2025-01-23 18:59:03 -0300

    creating projects

---
+++ C#_course/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float energia;
+            bool propulsorIzquierdo, propulsorDerecho;
+
+            Console.Write("Ingrese el nivel de energia: ");
+            energia = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("El propursor derecho esta en buene stado? (true/false)");
+            propulsorDerecho = Convert.ToBoolean(Console.ReadLine());
+
+            Console.Write("El propursor izquierdo esta en buene stado? (true/false)");
+            propulsorIzquierdo = Convert.ToBoolean(Console.ReadLine());
+
+            if(((((propulsorDerecho) && (propulsorIzquierdo)) == true) && (energia >= 75)) ||
+                    ((((propulsorDerecho || propulsorIzquierdo)) == true) && (energia >= 100)))
+                    {
+                Console.WriteLine("Puedes despegar");
+                     }   
+            else
+            {
+                Console.WriteLine("aborte mision");
+            }
+
+        }
+    }
+}
