7dea947ab754183c5d95cd9e994cec040af8a81d
Avila-Pato <p.avilaf1998@gmail.com>
2026-09-05 15:43:17 -0400

    Nuevos metodos

---
+++ cadenas/StringBuilder.cs
@@ -0,0 +1,13 @@
+class StringBuilder
+{
+    public static void Main(string[] args)
+    {
+        string numeros = "";
+        for (int i = 0; i < 100; i++)
+        {
+            numeros += i + " ";
+        }
+    Console.WriteLine(numeros); 
+    }
+
+}
---
+++ cadenas/TEXTING.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Nombre de la carpeta
+namespace cadenas
+{
+    // Nombre de la clase debe ser igual al nombre del archivo
+    public class TEXTING
+    {
+        public static void Main(string[] args)
+        {
+            
+        int numero1 = 10;
+        int numero2 = 30;
+
+        Console.WriteLine("El valor de la variable 1 es: " + numero1);
+        Console.WriteLine("El valor de la variable 2 es: " + numero2);
+
+        }
+    }
+}
